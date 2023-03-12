62e2d78e580ba7b99014ad29ff4926f1efd4ffc0
wojciech mierzejewski <55141520+mierzvoj@users.noreply.github.com>
2023-03-12 13:33:15 +0100

    BMI project commented

--- bmi/app/src/main/java/com/example/bmi/MainActivity.java
+++ bmi/app/src/main/java/com/example/bmi/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable; // for EditText event handling
 import android.text.TextWatcher; // EditText listener
-import android.widget.EditText; // for bill amount input
+import android.widget.EditText;
 import android.widget.TextView; // for displaying text
 
 import com.example.bmi.R;
@@ -13,30 +13,29 @@ import com.example.bmi.R;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    //all nesessary variables deifinition
     private double weight = 0.0;
     private double height = 0.15;
+    //TextViews definitions
     private TextView heightTextView;
     private TextView weightTextView;
 
 
     private TextView totalTextView;
 
-
+    //method onCreate, most important one
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState); // call superclass onCreate
         setContentView(R.layout.activity_main); // inflate the GUI
 
-
+//looking up TextViews in layout
         weightTextView = (TextView) findViewById(R.id.weightTextView);
         heightTextView = (TextView) findViewById(R.id.heightTextView);
-
-
         totalTextView = (TextView) findViewById(R.id.totalTextView);
         totalTextView.setText(String.format(0 + ""));
 
-        // set amountEditText's TextWatcher
+// setting TextWatchers for both weight and height fields
         EditText weightEditText =
                 (EditText) findViewById(R.id.weightEditText);
         weightEditText.addTextChangedListener(weightEditTextWatcher);
@@ -46,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
+    //method calculate, calculating BMI, setting totalTextView as result
     private void calculate() {
 
 
@@ -57,21 +56,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    // listener object for the EditText's text-changed events
+    // listeners object for TextViews weight and height text-changed events
     private final TextWatcher weightEditTextWatcher = new TextWatcher() {
-        // called when the user modifies the bill amount
+        // called when the user modifies weight or height field, or both
         @Override
         public void onTextChanged(CharSequence s, int start,
                                   int before, int count) {
 
-            try { // get bill amount and display currency formatted value
+            try {
                 weight = Double.parseDouble(s.toString()) / 100.0;
                 weightTextView.setText(String.format(weight + ""));
-            } catch (NumberFormatException e) { // if s is empty or non-numeric
+            } catch (NumberFormatException e) {
                 weightTextView.setText("");
                 weight = 0.0;
             }
-
+//recalculate on change
             calculate();
         }
 
@@ -93,12 +92,12 @@ public class MainActivity extends AppCompatActivity {
             try {
                 height = Double.parseDouble(s.toString()) / 100.0;
                 heightTextView.setText(String.format(height + ""));
-            } catch (NumberFormatException e) { // if s is empty or non-numeric
+            } catch (NumberFormatException e) {
                 heightTextView.setText("");
                 height = 0.0;
             }
-
-            calculate(); // update the tip and total TextViews
+//recalculate on change
+            calculate();
         }
 
         @Override
